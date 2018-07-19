2c269807f768d7c9ec3e6e8e97fd83dd1f40cf0c
Ryan <rvvergara@gmail.com>
2018-07-19 13:32:49 +0800

    Try to fix bug that happens when deploying to heroku

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
-import Series from "../../containers/Series";
+import Series from "../../containers/Series/index";
 import SingleSeries from "../../containers/SingleSeries";
 
 const Main = props => (
