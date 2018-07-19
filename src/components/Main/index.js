e2e4073628a4893494e035a5146d734691fb0f51
Ryan <rvvergara@gmail.com>
2018-07-19 13:35:43 +0800

    Revert change made in last commit to omit /index

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Switch, Route} from "react-router-dom";
-import Series from "../../containers/Series/index";
+import Series from "../../containers/Series";
 import SingleSeries from "../../containers/SingleSeries";
 
 const Main = props => (
